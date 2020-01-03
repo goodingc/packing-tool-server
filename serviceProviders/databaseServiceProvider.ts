d8db62bfa23426348d81dd191638746958369a7a
goodingc <callum.gooding11@gmail.com>
2020-01-03 11:19:33 +0000

    03/01 b

--- serviceProviders/databaseServiceProvider.ts
+++ serviceProviders/databaseServiceProvider.ts
@@ -3,21 +3,47 @@ import { createConnection } from "mysql2/promise";
 import { LocalLogger } from "../micro-node/LocalLogger";
 import { camel } from "case";
 
-export const databaseServiceProvider = new GlobalServiceProvider<DB>(
+export const databaseServiceProvider = new GlobalServiceProvider<DB | void>(
     "db",
     ["localLogger"],
     getGlobalServicePayload => {
         const localLogger = getGlobalServicePayload<LocalLogger>(
             "localLogger"
         ).tag("DB");
-        return createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_SCHEMA,
-            namedPlaceholders: true,
-            connectTimeout: 60000
-        })
+
+        const tryConnection = (tries = 1): Promise<any> => {
+            localLogger.info("connecting");
+            return new Promise((resolve, reject) => {
+                return createConnection({
+                    host: process.env.DB_HOST,
+                    user: process.env.DB_USER,
+                    password: process.env.DB_PASSWORD,
+                    database: process.env.DB_SCHEMA,
+                    namedPlaceholders: true,
+                    connectTimeout: 10000
+                })
+                    .then(resolve)
+                    .catch(reason => {
+                        if (tries < 100) {
+                            localLogger.warning(
+                                `Connection failed, tries ${tries}, reason:`,
+                                reason
+                            );
+                            setTimeout(
+                                () => resolve(tryConnection(tries++)),
+                                500
+                            );
+                        } else {
+                            localLogger.error(
+                                `Connection failed, aborting, reason:`,
+                                reason
+                            );
+                            reject(reason);
+                        }
+                    });
+            });
+        };
+        return tryConnection()
             .then(connection => {
                 localLogger.success("Connection successful");
                 const prepareData = ([results, meta]) => {
